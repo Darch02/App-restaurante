4770ba9598952598d7f1f886a87ec8c3d981996e
Darch02 <juanajaramillom@hotmail.com>
2025-02-27 18:59:32 -0500

    feat: se dieron estilos iniciales al popUp de las mesas

--- orderly/src/js/popUpMesa.js
+++ orderly/src/js/popUpMesa.js
@@ -3,7 +3,6 @@ async function PopUpMesa(navigateTo) {
     const htmlText = await response.text(); // Convierte la respuesta en texto HTML
     const container = document.createElement('div'); // Crear un contenedor temporal
     container.innerHTML = htmlText.trim(); // Insertar el HTML cargado
-    container.classList.add("popup-content");
     
     // container.querySelector(".popup-close").addEventListener("click", () => {
     //     document.body.removeChild(container.parentElement);
