ecff8b919bf52b59bc10671c9101a41f19885624
Darch02 <juanajaramillom@hotmail.com>
2025-03-03 12:30:17 -0500

    fix: añadir mesas según una variable

--- orderly/src/js/mesas.js
+++ orderly/src/js/mesas.js
@@ -5,16 +5,38 @@ async function Mesas(navigateTo) {
     const container = document.createElement('div'); // Crear un contenedor temporal
     container.innerHTML = htmlText.trim(); // Insertar el HTML cargado
 
-    const mesas= ['1','2','3','4','5' ];
+    // variable en la cual se guardaría toda la información de las mesas. esto despúes se debe guardar en local storage
+    const mesas= [{
+        nombre: 'Mesa 1',
+        Estado: 'activa',
+        sector: 'piso 1',
+        pedidos: [{
+            alimentos:[],
+            estado: 'activo'
+        }]
+    },
+    {
+        nombre: 'Mesa 2',
+        Estado: 'activa',
+        sector: 'piso 2',
+        pedidos: [{
+            alimentos:[],
+            estado: 'activo'
+        }]
+    }];
     const ContenedorMesas = container.querySelector('#contenedor-mesas');
     let ContenedorMesa;
 
-   
+   // pinta cada una de las mesas dentro de la variable mesas.
     mesas.forEach(mesa => {
         ContenedorMesa= document.createElement('div');
-        ContenedorMesa.classList.add(".mesa");
-        ContenedorMesa.classList.add('#mesa'+ mesa);
-        ContenedorMesa.addEventListener('click', () => navigateTo('/menu_aniadir'));
+        ContenedorMesa.innerHTML= `
+        <img class= "img-mesas" src="../src/assets/icono-mesa.svg" alt="imagen mesa"/>
+        <p>`+ mesa.nombre + `</p>
+    `
+        ContenedorMesa.classList.add("mesa");
+        ContenedorMesa.id = `mesa-${mesa}`;
+        ContenedorMesa.addEventListener('click', () => navigateTo('/menu_aniadir')); // la navegación del click debe cambiar segun el estado de la mesa
         ContenedorMesas.appendChild(ContenedorMesa);
     });
 
