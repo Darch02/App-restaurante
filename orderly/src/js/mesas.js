7dba3ff3e757a06de9ac569302c9f77501317256
Darch02 <juanajaramillom@hotmail.com>
2025-03-03 10:01:01 -0500

    feat: añadir mesas por medio de una variable ( todavía no funciona )

--- orderly/src/js/mesas.js
+++ orderly/src/js/mesas.js
@@ -1,19 +1,28 @@
 async function Mesas(navigateTo) {
+
     const response = await fetch('/Mesas.html'); // Carga el HTML externo
     const htmlText = await response.text(); // Convierte la respuesta en texto HTML
     const container = document.createElement('div'); // Crear un contenedor temporal
     container.innerHTML = htmlText.trim(); // Insertar el HTML cargado
+
+    const mesas= ['1','2','3','4','5' ];
+    const ContenedorMesas = container.querySelector('#contenedor-mesas');
+    let ContenedorMesa;
+
+   
+    mesas.forEach(mesa => {
+        ContenedorMesa= document.createElement('div');
+        ContenedorMesa.classList.add(".mesa");
+        ContenedorMesa.classList.add('#mesa'+ mesa);
+        ContenedorMesa.addEventListener('click', () => navigateTo('/menu_aniadir'));
+        ContenedorMesas.appendChild(ContenedorMesa);
+    });
 
 
     // navegación a la página de editar
     const edit= container.getElementsByClassName("icono-editar-grande")[0];
     edit.addEventListener('click',() => navigateTo('/editarMesas'));
 
-    // Agregar evento a cada mesa
-    const mesas = container.querySelectorAll(".mesa");
-    mesas.forEach(mesa => {
-        mesa.addEventListener('click', () => navigateTo('/menu_aniadir'));
-    });
 
     // const mesas = container.querySelectorAll(".mesa");
     // mesas.forEach(mesa => {
