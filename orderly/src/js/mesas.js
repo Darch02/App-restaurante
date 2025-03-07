e6a2376b7f300589117956a427e72253b3338e50
maryangela <maryangela.balcarcel@upb.edu.co>
2025-03-07 14:50:57 -0500

    pequeño cambio de estilo

--- orderly/src/js/mesas.js
+++ orderly/src/js/mesas.js
@@ -36,7 +36,7 @@ async function Mesas(navigateTo) {
         estado: 'activo',
         total: 0
       };
-    // localStorage.setItem("Mesas",JSON.stringify(mesas)); // se guarda en localstorage
+    //localStorage.setItem("Mesas",JSON.stringify(mesas)); // se guarda en localstorage
 
     /* todo el codigo anterior comentado es en el que se guardan las variables iniclamente. como ya las guardé una vez en en local storage
     no lo tengo que volver a hacer, ya que el almacenamieto es persistente*/
