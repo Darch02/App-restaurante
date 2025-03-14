2b28e29dab0dcb76be1ccaf7c080e15eb7a1dc2e
Darch02 <juanajaramillom@hotmail.com>
2025-03-14 16:56:42 -0500

    feat: edición del pop up de las mesas

--- orderly/src/js/popUpMesa.js
+++ orderly/src/js/popUpMesa.js
@@ -15,25 +15,42 @@ async function PopUpMesa(navigateTo) {
     const tituloMesa = container.getElementsByTagName('h1')[0];
     tituloMesa.innerHTML= MesaActual.nombre;
 
+    function pintarBotones(){
+        const contenedorBtns = container.querySelector('#contenedor-botones-mesa');
+        if(MesaActual.Estado === 'ocupada'){
+            contenedorBtns.innerHTML = `
+            <button id = 'añadir-pedido' class= "boton-negro">Añadir nuevo pedido</button>
+            <button id = 'finalizar-servicio' class= "boton-negro">Finalizar servicio</button>
+            `
+            const btnAñadirPedido = contenedorBtns.querySelector("#añadir-pedido");
+            btnAñadirPedido.addEventListener('click', () => {
+                if (container.parentElement) {
+                    document.body.removeChild(container.parentElement);
+                }
+                navigateTo('/menu_aniadir');
+            });
+            const btnFinalizarServicio = contenedorBtns.querySelector('#finalizar-servicio');
+            btnFinalizarServicio.addEventListener('click', () => {
+                if (container.parentElement) {
+                    document.body.removeChild(container.parentElement);
+                }
+                navigateTo('/factura');
+            });
+        }else{
+            contenedorBtns.innerHTML = `
+            <button id = 'comenzar-servicio' class= "boton-negro">Comenzar servicio</button>
+            <button id = 'eliminar-mesa' class= "boton-negro">Eliminar mesa</button>
+            `
+            const btnEliminarMesa = contenedorBtns.querySelector('#eliminar-mesa');
+            btnEliminarMesa.addEventListener('click', ()=>EliminarMesa());
+            const btnComenzarServicio = contenedorBtns.querySelector('#comenzar-servicio');
+            btnComenzarServicio.addEventListener('click', ()=> ComenzarServicio());
+        }
+    }
     // funcionalidad del botón de añadir pedido
-    const btnAñadirPedido = container.querySelector("#añadir-pedido");
-    btnAñadirPedido.addEventListener('click', () => {
-        if (container.parentElement) {
-            document.body.removeChild(container.parentElement);
-        }
-        navigateTo('/menu_aniadir');
-    });
+    
 
-    const btnFinalizarServicio = container.querySelector('#finalizar-servicio');
-    btnFinalizarServicio.addEventListener('click', () => {
-        if (container.parentElement) {
-            document.body.removeChild(container.parentElement);
-        }
-        navigateTo('/factura');
-    });
-
-    const btnEliminarMesa = container.querySelector('#eliminar-mesa')
-    btnEliminarMesa.addEventListener('click', ()=>{
+    function EliminarMesa(){
         let index = Mesas.findIndex(m => m.nombre === MesaActual.nombre);
         if (index !== -1) {
             Mesas.splice(index,1);
@@ -44,8 +61,23 @@ async function PopUpMesa(navigateTo) {
             }
             navigateTo('/mesas')
         }
-    });
+    }
 
+    function ComenzarServicio(){
+        let index = Mesas.findIndex(m => m.nombre === MesaActual.nombre);
+        if (index !== -1) {
+            Mesas[index].Estado = 'ocupada';
+            localStorage.setItem('Mesas', JSON.stringify(Mesas));
+            if (container.parentElement) {
+                document.body.removeChild(container.parentElement);
+            }
+            alert('se ha cambiado el estado de la mesa');
+            navigateTo('/mesas');
+        }
+    }
+    
+    pintarBotones();
+    
     return container;
 }
 export default PopUpMesa;
